f78988e1a4fe211df6bea811647f4ba2f6d048d4
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2022-12-27 17:26:58 -0300

    feat: add form to be able to handle new participants

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,4 +1,5 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 
 import { styles } from './styles';
 import { formatLocalizedDate } from '../../shared/utils';
@@ -6,10 +7,41 @@ import { formatLocalizedDate } from '../../shared/utils';
 export default function Home() {
   const date = formatLocalizedDate();
 
+  const [participantName, setParticipantName] = useState('');
+  const [participants, setParticipants] = useState<string[]>([]);
+
+  function handleParticipantAdd() {
+    const participantAlreadyAdded = participants.includes(participantName);
+
+    if (participantAlreadyAdded) {
+      return Alert.alert(
+        'Participante existe',
+        'Já existe um participante na lista com este nome'
+      );
+    }
+
+    setParticipants((prevState) => [...prevState, participantName]);
+    setParticipantName('');
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventTitle}>Lista de Participantes</Text>
       <Text style={styles.eventDate}>{date}</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+          value={participantName}
+          onChangeText={setParticipantName}
+          style={styles.input}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
