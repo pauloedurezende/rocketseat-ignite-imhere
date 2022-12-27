4b004fe7b6d63cf4c8cad8130db8c251a6a5400e
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2022-12-27 17:26:58 -0300

    feat: add form to be able to handle new participants

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,4 +16,30 @@ export const styles = StyleSheet.create({
     color: '#6b6b6b',
     fontSize: 16,
   },
+  form: {
+    flexDirection: 'row',
+    marginTop: 34,
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    borderRadius: 5,
+    color: '#fdfcfe',
+    flex: 1,
+    fontSize: 16,
+    height: 56,
+    marginRight: 12,
+    padding: 16,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#31cf67',
+    borderRadius: 5,
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 24,
+  },
 });
