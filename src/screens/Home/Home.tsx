f00c359fb2b04dda6fc1bbaacab1556310936cd6
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2022-12-27 16:34:01 -0300

    feat: add dinamic date based on device locale

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,12 +1,15 @@
 import { View, Text } from 'react-native';
 
 import { styles } from './styles';
+import { formatLocalizedDate } from '../../shared/utils';
 
 export default function Home() {
+  const date = formatLocalizedDate();
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventTitle}>Lista de Participantes</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de Dezembro de 2022</Text>
+      <Text style={styles.eventDate}>{date}</Text>
     </View>
   );
 }
