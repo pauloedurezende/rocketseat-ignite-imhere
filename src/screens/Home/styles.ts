9c087fcb02f1cd81dee2d1ed3abdb40d3f9a5eb6
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2022-12-27 12:15:56 -0300

    feat: add event name and date with styles

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#131016',
+    flex: 1,
+    padding: 24,
+  },
+  eventTitle: {
+    color: '#fdfcfe',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  eventDate: {
+    color: '#6b6b6b',
+    fontSize: 16,
+  },
+});
