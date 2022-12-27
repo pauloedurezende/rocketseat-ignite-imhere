eb5c4361dc45bb018de182a85c364134b0cc236a
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2022-12-27 12:15:56 -0300

    feat: add event name and date with styles

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from 'react-native';
 
+import { styles } from './styles';
+
 export default function Home() {
   return (
-    <View>
-      <Text>Home - Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventTitle}>Lista de Participantes</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Dezembro de 2022</Text>
     </View>
   );
 }
